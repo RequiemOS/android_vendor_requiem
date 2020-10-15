c17495fd5dc0bbec3788c887dacd7d79676371a4
coderastic <shubzz89@protonmail.ch>
2020-10-16 03:41:24 +0530

    build: Rrequiem bringup

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,22 +4,14 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# requiemOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.requiem.version=$(REQUIEM_VERSION) \
+    ro.requiem.releasetype=$(REQUIEM_BUILDTYPE) \
+    ro.requiem.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(REQUIEM_VERSION) \
+    ro.requiemlegal.url=https://requiem-os.com/legal
 
-# LineageOS Platform Display Version
+# requiemOS Platform Display Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
-
-# LineageOS Platform SDK Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.requiem.display.version=$(REQUIEM_DISPLAY_VERSION)
